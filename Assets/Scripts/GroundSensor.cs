162a2fc061b58aeb37a3288cef6b2d07167ae93d
VicCTS <victordm.cts@gmail.com>
2023-01-25 12:41:40 +0100

    animaciones

--- Assets/Scripts/GroundSensor.cs
+++ Assets/Scripts/GroundSensor.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class GroundSensor : MonoBehaviour
 {
+    private PlayerController controller;
     public bool isGrounded;
+
+    void Awake()
+    {
+        controller = GetComponentInParent<PlayerController>();
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         isGrounded = true;
+        controller.anim.SetBool("IsJumping", false);
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        isGrounded = true;
+        controller.anim.SetBool("IsJumping", false);
     }
 
     void OnTriggerExit2D(Collider2D other)
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,8 @@ public class PlayerController : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private Rigidbody2D rBody;
     private GroundSensor sensor;
+    public Animator anim;
+
     float horizontal;
 
     // Start is called before the first frame update
@@ -21,6 +23,7 @@ public class PlayerController : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
         rBody = GetComponent<Rigidbody2D>();
         sensor = GameObject.Find("GroundSensor").GetComponent<GroundSensor>();
+        anim = GetComponent<Animator>();
 
         playerHealth = 10;
         Debug.Log(texto);
@@ -36,15 +39,22 @@ public class PlayerController : MonoBehaviour
         if(horizontal < 0)
         {
             spriteRenderer.flipX = true;
+            anim.SetBool("IsRunning", true);
         }
         else if(horizontal > 0)
         {
             spriteRenderer.flipX = false;
+            anim.SetBool("IsRunning", true);
+        }
+        else
+        {
+            anim.SetBool("IsRunning", false);
         }
 
         if(Input.GetButtonDown("Jump") && sensor.isGrounded)
         {
             rBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            anim.SetBool("IsJumpinng", true);
         }
     }
 }
